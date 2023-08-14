070c51987dae94667c343346ae5273e2e1457aea
Kraga922 <138029547+Kraga922@users.noreply.github.com>
2023-08-14 11:59:39 -0400

    Update CarSupMulti.py

--- obstacleTesting1/controllers/CarSupMulti/CarSupMulti.py
+++ obstacleTesting1/controllers/CarSupMulti/CarSupMulti.py
@@ -4,28 +4,52 @@ import sys
 import os
 basedir = '../../..'
 sys.path.append(basedir)
-from json2overcome2 import json2overcome2 
 from InsertMultiSym2 import get_motors 
 import traceback
 import json
 
-def process_results(results_dir):
-    json2overcome = json2overcome2()
-    json_file_path = os.path.join(results_dir,'WebotSim2.json') 
-    #json2overcome.add_brackets_to_json_file(json_file_path)
-    file = json2overcome.openData(json_file_path)
-    #print(file)
-    dfTranslation= json2overcome.getTranslationDF(file)
-    #print(dfTranslation)
-    dfTranslation['translation'] = json2overcome.formatTranslationDF(dfTranslation)
-    #print(dfTranslation['translation'])
-    (num_obs, time) = json2overcome.obstaclePassed(dfTranslation, 'translation')
-    print(f'Passed {num_obs} obstacles in {time} time units')
-    result_obj = { 'num_obs': num_obs, 'time': int(time) }
-    result_file = os.path.join(results_dir,'result.json')
-    with open(result_file,'w') as fp:
-        json.dump(result_obj, fp, indent=2, separators=(',', ':'))
- 
+# def process_results(results_dir):
+#     json2overcome = json2overcome2()
+#     json_file_path = os.path.join(results_dir,'WebotSim2.json') 
+#     #json2overcome.add_brackets_to_json_file(json_file_path)
+#     file = json2overcome.openData(json_file_path)
+#     #print(file)
+#     dfTranslation= json2overcome.getTranslationDF(file)
+#     #print(dfTranslation)
+#     dfTranslation['translation'] = json2overcome.formatTranslationDF(dfTranslation)
+#     #print(dfTranslation['translation'])
+#     (num_obs, time) = json2overcome.obstaclePassed(dfTranslation, 'translation')
+#     print(f'Passed {num_obs} obstacles in {time} time units')
+#     result_obj = { 'num_obs': num_obs, 'time': int(time) }
+#     result_file = os.path.join(results_dir,'result.json')
+#     with open(result_file,'w') as fp:
+#         json.dump(result_obj, fp, indent=2, separators=(',', ':'))
+
+def obstaclePassed(time, value, obs_passed):
+    obs = []
+    if value >= 0.9:
+        if (obs_passed[0] == 3):
+            time = min(time,obs_passed[1]) 
+        obs = [3, time]
+    elif value >= 0.4: 
+        if (obs_passed[0] == 2):
+            time = min(time,obs_passed[1]) 
+        obs = [2, time]
+    elif value >= -0.1:
+        if (obs_passed[0] == 1):
+            time = min(time,obs_passed[1]) 
+        obs = [1, time]
+    else:
+        obs = [0, time]
+
+
+    #num_obs = obs[0]
+    #print(f'Passed {num_obs} obstacles in {time} time units')
+    # result_obj = { 'num_obs': num_obs, 'time': int(time) }
+    # result_file = os.path.join(results_dir,'result.json')
+    # with open(result_file,'w') as fp:
+    #     json.dump(result_obj, fp, indent=2, separators=(',', ':'))
+    return obs
 def run():
     try:
         headless = bool(os.getenv('WEBOTS_HEADLESS'))
@@ -34,10 +58,9 @@ def run():
             results_dir = f"{basedir}/obstacleTesting1/controllers/CarSup2/"
         timeout = os.getenv('WEBOTS_TIMEOUT')
         if timeout is None:
-            timeout = 5000  # magic number (?) from original version
+            timeout = 12000  # in ms
         else:
-            timeout = int(timeout)
-        timeout = int(timeout/65)
+            timeout = int(timeout) 
         
         #print(f'Headless: {headless}')
         #print(f'Results dir: {results_dir}')
@@ -45,26 +68,27 @@ def run():
 
         TIME_STEP = 64
         supervisor = Supervisor()
-        supervisor.silent = True
+
+        gps = supervisor.getDevice("gps")
+        #print(f"gps {gps}")
+        gps.enable(10)
+
         ds = []
         dsNames = ['ds_right', 'ds_left']
         timestep = int(supervisor.getBasicTimeStep())
 
-        # Export scene after 5 seconds
-        #camera = supervisor.getDevice("camera")
-        #camera.enable(timestep)
+
 
         supervisor.step(timestep) 
         html_save_path = os.path.join(results_dir,'WebotSim2.html') 
         supervisor.animationStartRecording(html_save_path)
-        #supervisor.movieStartRecording('WebotVid.mp4', 1280, 720, 0, 100, 1, False)
 
         for i in range(2):
             ds.append(supervisor.getDevice(dsNames[i]))
             ds[i].enable(TIME_STEP)
         wheelsNames = get_motors("../../../jsonWheelShapes/ConvJsonMultiSym2.json")
         wheels = []
-        for i in range(len(wheelsNames)): #need a way to find how many wheels there are
+        for i in range(len(wheelsNames)): 
             wheels.append(supervisor.getDevice(wheelsNames[i]))
             wheels[i].setPosition(float('inf'))
             wheels[i].setVelocity(0.0)
@@ -72,30 +96,41 @@ def run():
         #avoidObstacleCounter = 0
         time = 0
         #Main loop
+        obs_passed = [0,0]
         while supervisor.step(TIME_STEP) != -1: 
+
+            coordinates = gps.getValues()
+
+            #print(gps.getSpeed())
+            #print([round(number,3) for number in numbers])
+
             leftSpeed = 4.0
             rightSpeed = 4.0
 
+            time = int(supervisor.getTime() *1000)
+            #print(time)
             #body_position = body_node.getPosition()
             #print("BODY position:", body_position)
             #print(robot_body.getPosition())
-            if time > timeout:
+            obs_passed = obstaclePassed(time, coordinates[0], obs_passed)
+            #print(obs_passed)
+            #print(obs_passed)
+            if time > timeout or coordinates[0] > 1.1:
                 leftSpeed = 0
                 rightSpeed = 0
                 
-                #supervisor.movieStopRecording()
+                print(obstaclePassed(time, coordinates[0], obs_passed))
+
                 supervisor.animationStopRecording()
                 supervisor.simulationSetMode(supervisor.SIMULATION_MODE_PAUSE)
-                #supervisor.SIMULATION_MODE_PAUSE
-                #json2overcome
-                process_results(results_dir)
+
+
+                #process_results(results_dir)
                 
                 supervisor.simulationReset()
                 if headless:
                     print('Headless mode, quitting simulation...')
                     supervisor.simulationQuit(0)
-
-            time += 1
 
             for i in range (len(wheels)):
                 wheels[i].setVelocity(leftSpeed)     
@@ -106,10 +141,6 @@ def run():
             # wheels[4].setVelocity(leftSpeed)
             # wheels[5].setVelocity(rightSpeed)
 
-            # if supervisor.step(TIME_STEP) == 10:
-            #     supervisor.animationStopRecording()
-            #     supervisor.simulationResetPhysics()
-            #     supervisor.step(TIME_STEP) == -1
         #supervisor.worldRestart()
     except:
         traceback.print_exc()
@@ -119,7 +150,6 @@ def run():
             supervisor.simulationReset()
 
 if __name__ == "__main__":
-    #supervisor = Supervisor()
-    #pipeline2()
+
     run()
     
